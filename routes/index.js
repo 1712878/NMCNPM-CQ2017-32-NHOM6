c23cbb6f2aa2aeba06fba122fd1c4532e531c8f3
Nguyen Tho Tuan <1712878@student.hcmus.edu.vn>
2019-12-23 22:53:15 +0700

    design layout p1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/home/index',{pageTitle: 'Trang chủ'});
 });
-
+router.get('/change-user-info', function(req, res, next) {
+  res.render('pages/change-user-info/index', {pageTitle: 'Thay đổi thông tin người dùng'});
+});
+router.get('/forgot-password', function(req, res, next) {
+  res.render('pages/forgot-password/index', {pageTitle: 'Quên mật khẩu'});
+});
+router.get('/login', function(req, res, next) {
+  res.render('pages/login/index', {pageTitle: 'Đăng nhập'});
+});
+router.get('/manager-order', function(req, res, next) {
+  res.render('pages/manager-order/index', {pageTitle: 'Quản lí đơn hàng'});
+});
+router.get('/manager-product', function(req, res, next) {
+  res.render('pages/manager-product/index', {pageTitle: 'Quản lí sản phẩm'});
+});
+router.get('/manager-stall', function(req, res, next) {
+  res.render('pages/manager-stall/index', {pageTitle: 'Quản lí gian hàng'});
+});
+router.get('/manager-users', function(req, res, next) {
+  res.render('pages/manager-users/index', {pageTitle: 'Quản lí người dùng'});
+});
+router.get('/sales-detail', function(req, res, next) {
+  res.render('pages/sales-detail/index', {pageTitle: 'Thống kê bán hàng'});
+});
+router.get('/top10-detail', function(req, res, next) {
+  res.render('pages/top10-detail/index', {pageTitle: 'Thống kê top 10'});
+});
 module.exports = router;
