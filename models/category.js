0aff3e1b09c0a5da5f9ca443abc5e5b8a4a8fc53
Nguyen Van Hieu <elpulga246@gmail.com>
2019-12-30 17:26:11 +0700

    ! model category

--- models/category.js
+++ models/category.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
-const Book = new mongoose.Schema({
-    bookid: { type: String, required: true },
+const Category = new mongoose.Schema({
     name: { type: String, required: true },
-    author: { type: String, required: true },
-    publisher: { type: String },
-    cost: { type: Number },
-    quantity: { type: Number },
-    category: { type: String }
 })
-module.exports = mongoose.model('book', Book, 'book');
+module.exports = mongoose.model('category', Category, 'category');
--- routes/book.js
+++ routes/book.js
@@ -2,19 +2,21 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../config/passport');
 var Book = require('../models/book');
+var Category = require('../models/category');
 
 router.get('/book-summary', function(req, res, next) {
-    res.render('pages/book-summary/index', { pageTitle: 'Thống kê sách' });
+    let books = Book.find({}).lean();
+    res.render('pages/book-summary/index', { pageTitle: 'Thống kê sách', listbook: books });
 });
 router.get('/book-infomation', async function(req, res, next) {
     let minCost = req.query.minPrice;
     let maxCost = req.query.maxPrice;
     if (!minCost) minCost = Number.MIN_SAFE_INTEGER;
     if (!maxCost) maxCost = Number.MAX_SAFE_INTEGER;
-    let booksQuery = Book.find({ cost: { $lte: maxCost, $gte: minCost } }).lean();
+    let booksQuery = Book.find({ cost: { $lte: maxCost, $gte: minCost } });
     if (req.query.bookname)
         booksQuery = booksQuery.where('name').regex(req.query.bookname);
-    let books = await booksQuery.exec();
+    let books = await booksQuery.lean().exec();
     res.render('pages/book-infomation/index', { pageTitle: 'Tra cứu thông tin sách', listbook: books });
 });
 
