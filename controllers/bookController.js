842e6150c30bd74df582c7361b9b3227b7595eb3
tuan2012 <phampham1109@gmail.com>
2019-12-30 10:18:55 +0700

    +listbook foreach

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,7 @@
-const Book = require('../models/book');
-module.exports.bookInfoController = (req, res, next) => {
-    let book = await Book.find({});
-    res.render('/pages/book-infomation/index', {
+ const Book = require('../models/book');
+ module.exports.bookInfoController = (req, res, next) => {
+     let book = await Book.find({});
+     res.render('/pages/book-infomation/index', {
 
-    });
-}
+     });
+ }
--- routes/book.js
+++ routes/book.js
@@ -8,13 +8,12 @@ router.get('/book-summary', function(req, res, next) {
 });
 router.get('/book-infomation', async function(req, res, next) {
     let books = await Book.find({}).lean();
-
-
-    res.render('pages/book-infomation/index', { pageTitle: 'Tra cứu thông tin sách', books: books });
+    console.log(books);
+    res.render('pages/book-infomation/index', { pageTitle: 'Tra cứu thông tin sách',listbook: books});
 });
+
 router.get('/change-book-info', function(req, res, next) {
     res.render('pages/change-book-info/index', { pageTitle: 'Thay đổi thông tin sách' });
 });
-
 
 module.exports = router;
