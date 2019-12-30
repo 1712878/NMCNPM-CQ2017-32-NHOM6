e1ef4574942e8c6d26cf1e2e23774bda288f792d
Nguyen Van Hieu <elpulga246@gmail.com>
2019-12-30 20:53:59 +0700

    + trả về fail nếu không hợp lệ

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -3,16 +3,20 @@ var GiaoDich = require('../models/giaodich')
 module.exports.checkout = async(req, res, next) => {
     if (await Book.exists({ bookid: req.body.bookid })) {
         var b = await Book.findOne({ bookid: req.body.bookid });
-        if (b.quantity < req.body.quantity) res.render('pages/checkout/index', { pageTitle: 'Not enough quantity' });
-        else {
+        if (b.quantity < req.body.quantity) {
+            res.render('pages/checkout/index', { pageTitle: 'Not enough quantity' });
+            return;
+        } else {
             var newQuantity = b.quantity - req.body.quantity;
             await Book.findByIdAndUpdate(b.id, { quantity: newQuantity });
         }
         var cost = b.cost * req.body.quantity;
+        var today = new Date();
         var gd = new GiaoDich({
             bookId: req.body.bookid,
             quantity: req.body.quantity,
-            total: cost
+            total: cost,
+            time: today
         });
         await gd.save();
         res.render('pages/checkout/index', { pageTitle: 'Success' });
--- models/giaodich.js
+++ models/giaodich.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const GiaoDich = new mongoose.Schema({
     bookId: { type: String, required: true },
-    quantity: { type: String, required: true },
-    total: { type: Number, required: true }
+    quantity: { type: Number, required: true },
+    total: { type: Number, required: true },
+    time: { type: Date, required: true }
 });
 module.exports = mongoose.model('giaodich', GiaoDich, 'giaodich');
