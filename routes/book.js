02d84b0ebb6a1056eebb16a00c730e44188b6a51
tuan2012 <phampham1109@gmail.com>
2020-01-08 21:18:03 +0700

    + cập nhật thống kê sách

--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var Book = require('../models/book');
+let Order=require('../models/giaodich');
 var Category = require('../models/category');
 
 router.get('/book-summary', async function(req, res, next) {
     let books = await Book.find({}).lean();
-    res.render('pages/book-summary/index', { pageTitle: 'Thống kê sách', listbook: books });
+    let orders=await Order.find({});
+    console.log(orders);
+    let sumtotal=0,sumbook=0, sumkho=0;
+
+    for(let i=0;i<books.length;i++)
+    {
+        sumkho=sumkho+books[i].quantity;
+    }
+    for(let i=0;i<(await orders).length;i++)
+    {
+        sumtotal=sumtotal+orders[i].total;
+        sumbook=sumbook+orders[i].quantity;
+    }
+    res.render('pages/book-summary/index', { pageTitle: 'Thống kê sách', listbook: books ,sumtotal,sumkho,sumbook});
 });
 router.get('/book-infomation', async function(req, res, next) {
     let minCost = req.query.minPrice;
